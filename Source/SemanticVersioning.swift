a3370af6d8cc1836902fd26ddcf889384702c62a
AlexanderNey <alexander.ney@me.com>
2016-05-05 23:24:50 +0100

    SwiftLint autocorrect for SemanticVersioning.swift

--- Source/SemanticVersioning.swift
+++ Source/SemanticVersioning.swift
@@ -24,8 +24,7 @@
 
 import Foundation
 
-public protocol SemanticVersion: Comparable
-{
+public protocol SemanticVersion: Comparable {
     var major: Int { get }
     var minor: Int { get }
     var patch: Int { get }
@@ -37,60 +36,55 @@ public protocol SemanticVersion: Comparable
 /**
 *  Implements Sematic Version specification 2.0.0
 */
-public struct Version: SemanticVersion, CustomStringConvertible
-{
+public struct Version: SemanticVersion, CustomStringConvertible {
     public var major: Int
     public var minor: Int
     public var patch: Int
     public var preReleaseIdentifier: [String]
     public var buildMetadataIdentifier: [String]
     public var isPrerelease: Bool { return !self.preReleaseIdentifier.isEmpty }
-    
+
     /**
-    - returns: returns a SemanticVersion defining the specification that is implemented (http://semver.org/spec/v2.0.0.html)
+    - returns:  returns a SemanticVersion defining the specification
+                that is implemented (http://semver.org/spec/v2.0.0.html)
     */
     public static var specification: Version {
         return Version(major: 2, minor: 0, patch: 0)
     }
-    
+
     public var description: String {
         var versionString = "\(self.major).\(self.minor).\(self.patch)"
-        
-        if !self.preReleaseIdentifier.isEmpty
-        {
+
+        if !self.preReleaseIdentifier.isEmpty {
             versionString += "-" + self.preReleaseIdentifier.joinWithSeparator(".")
         }
-        
-        if !self.buildMetadataIdentifier.isEmpty
-        {
+
+        if !self.buildMetadataIdentifier.isEmpty {
             versionString += "+" + self.buildMetadataIdentifier.joinWithSeparator(".")
         }
-        
+
         return versionString
     }
-    
-    public init(major: Int, minor: Int = 0, patch: Int = 0, preReleaseIdentifier: [String] = [], buildMetadataIdentifier: [String] = [])
-    {
+
+    public init(major: Int, minor: Int = 0, patch: Int = 0, preReleaseIdentifier: [String] = [], buildMetadataIdentifier: [String] = []) {
         self.major = major
         self.minor = minor
         self.patch = patch
-        
+
         self.preReleaseIdentifier = preReleaseIdentifier
         self.buildMetadataIdentifier = buildMetadataIdentifier
     }
-    
-    public init<T: SemanticVersion>(version: T)
-    {
+
+    public init<T: SemanticVersion>(version: T) {
         self.major = version.major
         self.minor = version.minor
         self.patch = version.patch
-        
+
         self.preReleaseIdentifier = version.preReleaseIdentifier
         self.buildMetadataIdentifier = version.buildMetadataIdentifier
     }
-    
-    private init()
-    {
+
+    private init() {
         self = Version(major: 0)
     }
 }
@@ -98,89 +92,61 @@ public struct Version: SemanticVersion, CustomStringConvertible
 // MARK: comparison
 
 infix operator ≈ { associativity left precedence 140 }
-func ≈ <T: SemanticVersion, U: SemanticVersion>(left: T, right: U) -> Bool
-{
+func ≈ <T: SemanticVersion, U: SemanticVersion>(left: T, right: U) -> Bool {
     return  (left.major == right.major) &&
             (left.minor == right.minor) &&
             (left.patch == right.patch)
 }
 
 infix operator !≈ { associativity left precedence 140 }
-func !≈ <T: SemanticVersion, U: SemanticVersion>(left: T, right: U) -> Bool
-{
+func !≈ <T: SemanticVersion, U: SemanticVersion>(left: T, right: U) -> Bool {
     return  !(left ≈ right)
 }
 
-public func == <T: SemanticVersion, U: SemanticVersion>(left: T, right: U) -> Bool
-{
+public func == <T: SemanticVersion, U: SemanticVersion>(left: T, right: U) -> Bool {
     return  left ≈ right &&
             (left.preReleaseIdentifier == right.preReleaseIdentifier)
 }
 
 
-public func < <T: SemanticVersion, U: SemanticVersion>(left: T, right: U) -> Bool
-{
-    if left.major < right.major
-    {
+public func < <T: SemanticVersion, U: SemanticVersion>(left: T, right: U) -> Bool {
+    if left.major < right.major {
         return true
-    }
-    else if left.major == right.major
-    {
-        if left.minor < right.minor
-        {
+    } else if left.major == right.major {
+        if left.minor < right.minor {
             return true
-        }
-        else if left.minor == right.minor
-        {
-            if left.patch < right.patch
-            {
+        } else if left.minor == right.minor {
+            if left.patch < right.patch {
                 return true
-            }
-            else if left.patch == right.patch
-            {
-                if left.isPrerelease && !right.isPrerelease
-                {
+            } else if left.patch == right.patch {
+                if left.isPrerelease && !right.isPrerelease {
                     return true
-                }
-                else if left.isPrerelease && right.isPrerelease
-                {
+                } else if left.isPrerelease && right.isPrerelease {
                     // Compare prerelease identifier
                     let identifiers = Zip2Sequence(left.preReleaseIdentifier, right.preReleaseIdentifier)
-                    for pair in identifiers
-                    {
+                    for pair in identifiers {
                         let numericLeft = Int(pair.0)
                         let numericRight = Int(pair.1)
-                        
-                        if let numericLeft = numericLeft, numericRight = numericRight where numericLeft != numericRight
-                        {
+
+                        if let numericLeft = numericLeft, numericRight = numericRight where numericLeft != numericRight {
                             // identifiers consisting of only digits are compared numerically
                             return numericLeft < numericRight
-                        }
-                        else if numericLeft != nil && numericRight == nil
-                        {
+                        } else if numericLeft != nil && numericRight == nil {
                             return true // Numeric identifiers always have lower precedence than non-numeric identifiers
-                        }
-                        else if numericLeft == nil && numericRight != nil
-                        {
+                        } else if numericLeft == nil && numericRight != nil {
                             return false
-                        }
-                        else if pair.0 != pair.1
-                        {
+                        } else if pair.0 != pair.1 {
                             // identifiers with letters or hyphens are compared lexically in ASCII sort order
                             return pair.0 < pair.1
                         }
                     }
-                    
+
                     // A larger set of pre-release fields has a higher precedence than a smaller set, if all of the preceding identifiers are equal
                     return left.preReleaseIdentifier.count < right.preReleaseIdentifier.count
                 }
-            }
-            else { return false }
-        }
-        else { return false }
+            } else { return false }
+        } else { return false }
     }
-    
+
     return false
 }
-
-
